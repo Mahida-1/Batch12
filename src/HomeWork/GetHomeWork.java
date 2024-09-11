693688c2d60dfec9cd26fd22551067d4fd5b54e4
mahid <mahid@LAPTOP-O0I9OS8B>
2024-09-10 19:06:33 -0700

    remove

--- src/HomeWork/GetHomeWork.java
+++ src/HomeWork/GetHomeWork.java
@@ -2,15 +2,4 @@ package HomeWork;
 
 public class GetHomeWork {
 
-	public void display () {
-		System.out.println ("john");
-	}
-	String firstName = "john";
-	String lasName = "cena";
-	String city = "Reston";
-
-	/*** 
-	this is coming from GitHub
-	java is fun 
-	*/
 }
