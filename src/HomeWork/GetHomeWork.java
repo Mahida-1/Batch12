3333ccdbaf5b19c1cb8260bab1f673270144f7ef
mahid <mahid@LAPTOP-O0I9OS8B>
2024-09-10 18:52:43 -0700

    mmm

--- src/HomeWork/GetHomeWork.java
+++ src/HomeWork/GetHomeWork.java
@@ -2,15 +2,10 @@ package HomeWork;
 
 public class GetHomeWork {
 
-	public static void main (String[] args) {
-		
-		System.out.println ("Batch 12 Git Class Lecture");
-		int age = 12;
-		String name = "john";
-		// this is coming from github
+	public void display () {
+		System.out.println ("john");
 	}
-	public static void dd (int a, int b) {
-		System.out.println (a+b);
-	}
-	
+	String firstName = "john";
+	String lasName = "cena";
+	String city = "Reston";
 }
