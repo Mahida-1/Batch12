ed26f199a53204653d8aff17ba8f981d8714427b
Mahida-1 <mahida.hamid25@gmail.com>
2024-09-10 17:50:15 -0700

    Update GetHomeWork.java

--- src/HomeWork/GetHomeWork.java
+++ src/HomeWork/GetHomeWork.java
@@ -5,8 +5,9 @@ public class GetHomeWork {
 	public static void main (String[] args) {
 		
 		System.out.println ("Batch 12 Git Class Lecture");
-		
-		
+		int age = 12;
+		String name = "john";
+		// this is coming from github
 	}
 	public static void dd (int a, int b) {
 		System.out.println (a+b);
